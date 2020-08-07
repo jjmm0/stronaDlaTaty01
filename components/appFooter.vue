6cf5ab196d87611b0fd42d9bf5319e2e3a79d094
jjmm0 <jedrzej.miga@gmail.com>
2020-08-07 16:21:59 +0200

    asd

---
+++ components/appFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Footer -->
+  <footer class="footer">
+       <center> &copy; 2020 Copyright: <a href="https://dobrerzeczy.ngo/"> dobrerzeczy </a> </center>
+  </footer>
+  <!-- Footer -->
+</template>
+
+<style scoped> 
+
+
+.footer {
+    background-color: black;
+    color: whitesmoke;
+    flex-shrink: 0;
+    height: 20px;
+    display: block;
+    position: static;
+    
+}
+*::before, *::after{
+  height: 100%;
+}
+
+html, body {
+  height: 100%;
+}
+
+
+</style>
---
+++ components/appHeader.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div class="navblock">
+  <div class="wrapper2">
+
+    <a href="/main" class="main"><p class="main">Główna</p></a>
+    <a href="/info" class="info"><p class="info">Informacje</p></a>
+    <a href="/gallery" class="gallery"><p class="gallery">Galeria</p></a>
+    <a href="/history" class="history"><p class="history">Ostatnie zlecenia</p></a>
+    <a href="/contact" class="contact"><p class="contact">Kontakt</p></a>
+
+  </div>
+</div>
+
+</template>
+
+<style scoped lang="css" >
+  @import ".././styles/headerStyles.css";
+</style>
+
